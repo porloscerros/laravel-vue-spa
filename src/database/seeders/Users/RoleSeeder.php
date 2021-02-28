425c15ac1a75e8353e551e04d9064e430f99a463
porloscerros <porloscerros@gmail.com>
2021-02-28 12:07:21 -0300

    Seed Users and Roles

--- src/database/seeders/Users/RoleSeeder.php
+++ src/database/seeders/Users/RoleSeeder.php
@@ -22,17 +22,18 @@ class RoleSeeder extends Seeder
 
         User::where('username', 'anakin')->select('id')->first()->roles()->sync($admin_role->id);
 
-        $jedi_role  = Role::firstOrCreate(
-            ['keyname' => 'jedi'],
-            ['name' => 'Jedi']
-        );
-
-        User::where('username', 'luke')->select('id')->first()->roles()->sync($jedi_role->id);
+//        $jedi_role  = Role::firstOrCreate(
+//            ['keyname' => 'jedi'],
+//            ['name' => 'Jedi']
+//        );
+//
+//        User::where('username', 'luke')->select('id')->first()->roles()->sync($jedi_role->id);
 
         $clone_role  = Role::firstOrCreate(
             ['keyname' => 'clone'],
             ['name' => 'Clone']
         );
+        User::where('username', 'clone0')->select('id')->first()->roles()->sync($clone_role->id);
     }
 }
 
--- src/database/seeders/Users/UserSeeder.php
+++ src/database/seeders/Users/UserSeeder.php
@@ -19,19 +19,26 @@ class UserSeeder extends Seeder
             [ 'username' => 'anakin' ],
             [
                 'name' => 'Anakin Skywalker',
-                'username' => 'anakin',
                 'email' => 'darthvader@deathstar.ds',
                 'password' => Hash::make('4nak1n'),
                 'email_verified_at' => now()
             ]
         );
+//        User::firstOrCreate(
+//            [ 'username' => 'luke' ],
+//            [
+//                'name' => 'Luke Skywalker',
+//                'email' => 'luke@jedi.force',
+//                'password' => Hash::make('T4too1ne'),
+//                'email_verified_at' => now()
+//            ]
+//        );
         User::firstOrCreate(
-            [ 'username' => 'luke' ],
+            [ 'username' => 'clone0' ],
             [
-                'name' => 'Luke Skywalker',
-                'username' => 'luke',
-                'email' => 'luke@jedi.force',
-                'password' => Hash::make('T4too1ne'),
+                'name' => 'Clone 0',
+                'email' => 'Clone0@empire.emp',
+                'password' => Hash::make('secret'),
                 'email_verified_at' => now()
             ]
         );
